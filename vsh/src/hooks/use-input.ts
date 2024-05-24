1525c0f4c00486f6928cdd80f3a7078215790bb0
Madeline H <maddie@rykan.net>
2024-05-24 15:30:38 +0100

    Fixes and improvements to useInput

--- vsh/src/hooks/use-input.ts
+++ vsh/src/hooks/use-input.ts
@@ -1,20 +1,23 @@
-import { useEffect, useRef } from "preact/hooks";
+import { useCallback, useEffect, useRef } from "preact/hooks";
 
 /**
+ * Effectively a combination of useEffect and useCallback, streamlined
+ * for the repetitive need to handle keypresses all over the place
+ * in a consistent manner.
+ * 
  * TODO: `keyup` handling?
  * @param active Whether the current screen/component is active/focused.
  * @param callback Handler function for button press events.
  * @param deps Dependencies like you would use in a call to `useEffect()`
  */
 export function useInput(active: boolean, callback: (button: string) => void, deps: Array<any>) {
-	const user_handler = useRef(callback);
-	user_handler.current = callback;
+	const user_handler = useCallback(callback, deps);
 	useEffect(() => {
 		if (!active) return;
 		function handler(e: KeyboardEvent) {
-			user_handler.current(e.key);
+			user_handler(e.key);
 		}
 		window.addEventListener("keydown", handler);
 		return () => { window.removeEventListener("keydown", handler); };
-	}, [active, ...deps]);
+	}, [active, user_handler]);
 }
