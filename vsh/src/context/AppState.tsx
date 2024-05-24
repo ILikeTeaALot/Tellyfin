2a5284b26801813feb44d60e86cfa667ce2529fc
Madeline H <maddie@rykan.net>
2024-05-24 16:08:03 +0100

    add helper context to allow app mode switching from anywhere

--- vsh/src/context/AppState.tsx
+++ vsh/src/context/AppState.tsx
@@ -2,3 +2,5 @@ import { createContext } from "preact";
 import { AppState } from "../AppStates";
 
 export const AppMode = createContext(AppState.Home);
+
+export const SwitchMode = createContext((state: AppState) => {});
