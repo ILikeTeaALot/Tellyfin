d5cb0080a4a0cea39562eb5ca0ac9c5639bb476e
Madeline H <maddie@rykan.net>
2024-05-24 16:25:16 +0100

    near-total overhaul of Video control

--- vsh/src/screens/Video.tsx
+++ vsh/src/screens/Video.tsx
@@ -7,159 +7,174 @@ import { refresh_mpv } from "../util/functions";
 import { Timeline } from "../components/Timeline";
 import VideoState, { PlaybackStatus, VideoContextType } from "../context/VideoContext";
 import { PauseIcon, PlayIcon, StopIcon } from "../Icons";
-import { useSWRConfig } from "swr";
 import { useInput } from "../hooks";
 import { SceneSearch } from "../components/SceneSearch";
+import { Dialog } from "../components/Dialog";
+
+const withoutSceneSearch: VideoFunction[] = ["SubtitleOptions", "Display"];
+
+const withSceneSearch: VideoFunction[] = ["SceneSearch", ...withoutSceneSearch];
 
 export function Video(props: ScreenProps) {
 	// Props
-	const { active, change_state } = props;
+	const { active, change_state: changeState } = props;
 	// Context
 	const state = useContext(VideoState);
-	// Mutator
-	const { mutate: _mutate } = useSWRConfig();
-	const mutate = useRef(_mutate);
-	mutate.current = _mutate;
-	const changeState = useRef(change_state);
-	changeState.current = change_state;
+	const playback_status = state.status.playback_status;
+	const time = state.position.time;
 	// State
 	const [controlPanelActive, setPanelActive] = useState(false);
 	const [displayVisible, setDisplayVisible] = useState(false); // Using terminology from the PS3.
 	const [seekActive, setSeekActive] = useState(false);
 	const [sceneSearchActive, setSceneSearchActive] = useState(false);
-	// Panel/Overlay/etc visibility.
-	const anyToolVisible = useRef(false);
-	anyToolVisible.current = controlPanelActive || displayVisible || seekActive || sceneSearchActive;
-	const panelVisibleRef = useRef(controlPanelActive);
-	panelVisibleRef.current = controlPanelActive;
-	useEffect(() => {
-		if (state.status.playback_status == PlaybackStatus.Stopped) {
-			changeState.current(AppState.Home);
-		}
-	}, [state.status.playback_status, active]);
-	function closeAllPanels() {
+	const [exitConfirmActive, setExitConfirmActive] = useState(false);
+	const closeAllPanels = useCallback(() => {
 		setPanelActive(false);
 		setSeekActive(false);
 		setDisplayVisible(false);
 		setSceneSearchActive(false);
-	};
-	useInput(active, (button) => {
-		// console.log(button);
-		if (anyToolVisible.current) {
-			switch (button) {
-				case "Back":
-				case "Backspace":
-					setPanelActive(false);
-					setSceneSearchActive(false);
-					setSeekActive(false);
-					return;
-			}
+		setExitConfirmActive(false);
+	}, []);
+	useEffect(() => {
+		if (playback_status == PlaybackStatus.Stopped) {
+			changeState(AppState.Home);
 		}
+	}, [playback_status, active, changeState]);
+	useInput(active && (controlPanelActive || seekActive || sceneSearchActive), (button) => {
+		switch (button) {
+			case "Back":
+			case "Backspace":
+				setPanelActive(false);
+				setSceneSearchActive(false);
+				setSeekActive(false);
+				return;
+		}
+	}, []);
+	useInput(active && !(seekActive || sceneSearchActive || exitConfirmActive), (button) => {
+		// If no tool other than control panel is open.
 		switch (button) {
 			case "t": // "t" for triangle
 			case "Y":
 				setPanelActive(v => !v);
-				refresh_mpv();
+				// refresh_mpv();
 				break;
 			case "d": // "d" for display
 			case "Select":
 				setDisplayVisible(v => !v);
 				refresh_mpv();
 				break;
+		}
+	}, []);
+	useInput(active && !controlPanelActive && !exitConfirmActive, (button) => {
+		switch (button) {
 			case "x": // Self explanatory
 			case "X":
 				closeAllPanels();
 				setSceneSearchActive(true);
 				break;
+		}
+	}, [closeAllPanels]);
+	useInput(active && controlPanelActive, (button) => {
+		switch (button) {
 			case "Back":
 			case "Backspace":
-				if (panelVisibleRef.current) {
-					setPanelActive(false);
-				} else {
-					changeState.current(AppState.Home);
-				}
-				refresh_mpv();
+				setPanelActive(false);
 				break;
 		}
-		if (!panelVisibleRef.current && !anyToolVisible.current) {
-			switch (button) {
-				case "Enter":
-					invoke("transport_command", { function: "TogglePlay" }).then(() => {
-						refresh_mpv();
-					}, () => { });
-					break;
-				case "PadLeft":
-					invoke("seek", { mode: "relative", seconds: -10 }).then(() => {
-						console.log("Succeeded!");
-						refresh_mpv();
-					}, () => { });
-					break;
-				case "PadRight":
-					invoke("seek", { mode: "relative", seconds: 10 }).then(() => {
-						console.log("Succeeded!");
-						refresh_mpv();
-					}, () => { });
-					break;
-				default:
-					break;
-
-			}
+	}, []);
+	useInput(active && !controlPanelActive && !seekActive && !sceneSearchActive && !exitConfirmActive, (button) => {
+		switch (button) {
+			case "Back":
+			case "Backspace":
+				setExitConfirmActive(true);
+				break;
+			case "Enter":
+				invoke("transport_command", { function: "TogglePlay" }).then(() => {
+					refresh_mpv();
+				}, () => { });
+				break;
+			case "PadLeft":
+				invoke("seek", { mode: "relative", seconds: -10 }).then(() => {
+					console.log("Succeeded!");
+				}, () => { });
+				break;
+			case "PadRight":
+				invoke("seek", { mode: "relative", seconds: 10 }).then(() => {
+					console.log("Succeeded!");
+				}, () => { });
+				break;
+			default:
+				break;
 		}
 	}, []);
 	useEffect(() => {
 		if (!active) {
 			closeAllPanels();
 		}
-	}, [active]);
-	const on_time_jump = (seconds: number) => {
+	}, [active, closeAllPanels]);
+	const on_submit = useCallback((seconds: number) => {
 		if (seconds >= 0) invoke("seek", { mode: "absolute", seconds });
-	};
-	const on_cancel = () => {
 		closeAllPanels();
-	};
-	const handle_panel_action = (action: string) => {
+	}, [closeAllPanels]);
+	const on_dialog_submit = useCallback((confirmed: boolean) => {
+		if (confirmed) {
+			invoke("transport_command", { function: "Stop" });
+			changeState(AppState.Home);
+		} else {
+			setExitConfirmActive(false);
+		}
+	}, [changeState]);
+	const on_cancel = useCallback(() => {
+		closeAllPanels();
+	}, [closeAllPanels]);
+	const handle_panel_action = useCallback((action: string) => {
 		switch (action) {
 			case "SceneSearch":
-				setPanelActive(false);
+				closeAllPanels();
 				setSceneSearchActive(true);
-				setPanelActive(false);
 				return;
 			case "GoTo":
-				setPanelActive(false);
+				closeAllPanels();
 				setSeekActive(true);
-				setPanelActive(false);
 				return;
 			case "SubtitleOptions":
-				setPanelActive(false);
+				invoke("transport_command", { function: action });
+				// setPanelActive(false);
 				return;
 			case "AudioOptions":
-				setPanelActive(false);
+				// setPanelActive(false);
 				return;
 			case "Display":
 				setDisplayVisible(v => !v);
 				return;
 		}
-	};
+	}, [closeAllPanels]);
 	return (
-		<div id="video-root" style={{ opacity: active ? 1 : 0 }}>
-			<VideoControlPanel active={controlPanelActive} onAction={handle_panel_action} />
-			<div className="video-info" style={{ opacity: displayVisible ? 1 : 0 }}>
-				<span>{state.jellyfin_data?.Name ?? state.title ?? state.filename ?? null}</span>
+		<div id="video-root">
+			<VideoControlPanel active={controlPanelActive} onAction={handle_panel_action} options={
+				state.jellyfin_data?.Chapters ? withSceneSearch : withoutSceneSearch
+			} />
+			<div className="video-info" style={{ opacity: displayVisible || sceneSearchActive || seekActive ? 1 : 0 }}>
+				{displayTitle(state)}
 			</div>
 			<div className="video-timeline" style={{ opacity: displayVisible ? 1 : 0 }}>
 				<div style={{ flexGrow: 1 }} />
-				<Timeline {...state.position.time} />
+				<Timeline realtime position={time.position} duration={time.duration} />
 			</div>
-			{state.jellyfin_data ? <SceneSearch active={sceneSearchActive} data={state.jellyfin_data} default={state.position.chapter ?? 0} onCancel={on_cancel} onSubmit={on_time_jump} /> : null}
-			<div id="playback-status-indicator" className={PlaybackStatus[state.status.playback_status]} style={{ opacity: displayVisible || state.status.playback_status != PlaybackStatus.Playing ? 1 : 0, transitionDelay: displayVisible ? "0s" : state.status.playback_status == PlaybackStatus.Playing ? "5s" : "0ms" }}>
-				{statusIcon(state)}
+			{state.jellyfin_data ? <SceneSearch active={sceneSearchActive} data={state.jellyfin_data} default={state.position.chapter || 0} onCancel={on_cancel} onSubmit={on_submit} /> : null}
+			<div id="playback-status-indicator" className={PlaybackStatus[playback_status]} style={{
+				opacity: (displayVisible || playback_status != PlaybackStatus.Playing) && active ? 1 : 0,
+				transitionDelay: displayVisible ? "0s" : playback_status == PlaybackStatus.Playing ? "5s" : "0ms",
+			}}>
+				{statusIcon(playback_status)}
 			</div>
+			<Dialog active={exitConfirmActive} onSubmit={on_dialog_submit} onCancel={on_cancel}>Do you want to stop playback?</Dialog>
 		</div>
 	);
 }
 
-function statusIcon(state: VideoContextType) {
-	switch (state.status.playback_status) {
+function statusIcon(playback_status: PlaybackStatus) {
+	switch (playback_status) {
 		case PlaybackStatus.Stopped:
 			return (
 				<StopIcon width="64px" height="64px" style={{ translate: "0px -16px" }} />
@@ -175,4 +190,29 @@ function statusIcon(state: VideoContextType) {
 		default:
 			return "Status Error";
 	}
+}
+
+function displayTitle(state: VideoContextType) {
+	if (state.jellyfin_data) {
+		if (state.jellyfin_data.Type) {
+			switch (state.jellyfin_data.Type) {
+				case "Episode":
+					return <span>{state.jellyfin_data.SeriesName} {state.jellyfin_data.SeasonName}, Episode {state.jellyfin_data.IndexNumber}: {state.jellyfin_data.Name ?? state.title ?? state.filename ?? null}</span>;
+				case "Movie":
+					return <span>{state.jellyfin_data.Name ?? state.title ?? state.filename ?? null}</span>;
+				case "MusicVideo":
+				case "Program":
+				case "Recording":
+				case "Season":
+				case "Series":
+				case "Trailer":
+				case "Video":
+					break;
+				default:
+					break;
+			}
+		}
+		return <span>{state.jellyfin_data?.Name ?? state.title ?? state.filename ?? null}</span>;
+	}
+	return <span>{state.title ?? state.filename ?? null}</span>;
 }
