3718c84fd79dbf172827ba2f734f8b7d73327bb7
Madeline H <maddie@rykan.net>
2024-05-24 15:45:30 +0100

    fix the most painful logic mistake of my life

---
+++ vsh/Notes.md
@@ -0,0 +1,31 @@
+# Notes
+
+As of 14:08 on 23/05/2024, I have determined that there is a memory leak somewhere within the `Home` or `GamepadContext` components.
+
+I systematically checked enabling and disabling different parts of the interface:
+
+ - `MpvStateProvider` was first, and works fine,
+ - `GamepadContext` was second, and seemed to cause some issue,
+ - `SWRConfig` was next, and works fine, which is unsurprising because it's just a config provider.
+ - `AppInner` modified to return `null` was fine, which led me to test
+ - `AppInner` with only `AppMode.Provider`, `SwitchMode.Provider`, and the two `<div class="background .." ... />`s, which were all fine in combination.
+ - `AppInner` as above + `StatusBar`, which again worked fine, although caused a cycle of building up around 4-5MB before a milk 1% spike in CPU usage and a drop back to the starting memory amount (presumably GC running on items related to the `setInterval`.)
+ - `AppInner` as above + `Video`, which I was pleasantly surprised to find worked without issue, however without access to `Home` there is not really a way to test with media actually playing.
+
+∴ the only components remaining to be checked are `Home` and `GamepadContext`, ergo one of them must be the root of the leak (OR it is something with MS Edge WebView2)
+
+## Update 14:19
+
+`Home` with all 3 screen types set to return `null` (i.e. all hooks still executing, but only `<div id="home-root">{...}</div>` being rendered with no contents), there appears to be no leak.
+
+## Update 15:30
+
+When testing with video actually playing, something within `Video` appears to be a/the cause of the leak.
+
+## Update 17:00
+
+Still having leak issues... I no longer have much of a clue why...
+
+## Update 18:00
+
+Turns out I had mixed up a value in milliseconds and a value in seconds in an update function... That was annoying.
--- vsh/src/components/Timeline.tsx
+++ vsh/src/components/Timeline.tsx
@@ -7,56 +7,64 @@ export type TimelineProps = {
 	position: number;
 	duration: number | null;
 	mini?: boolean;
+	realtime?: boolean;
 };
 
-const UPDATE_FREQUENCY = 6; // 6Hz
+// const UPDATE_FREQUENCY = 6; // 6Hz
 
 export function Timeline(props: TimelineProps) {
-	const { position, duration, mini } = props;
-
-	const remaining = duration ? duration - position : -0;
-
+	const { position, duration: duration_seconds, mini, realtime } = props;
+	
 	const mpvState = useContext(VideoState);
-
+	
 	const playState = mpvState.status.playback_status;
-
+	
+	const play_state = useRef<PlaybackStatus>(playState);
+	
 	const frame = useRef<number>(-1);
-	const last = useRef<number>(0);
-	const elapsed = useRef<number>(position);
+	const last_ms = useRef<number>(0);
+	const elapsed_ms = useRef<number>(position * 1000);
+	
+	const [elapsed_seconds, setElapsedSeconds] = useState(position ?? 0);
 
-	const [elapsedSeconds, setElapsedSeconds] = useState(elapsed.current);
+	const remaining_seconds = duration_seconds ? duration_seconds - elapsed_seconds : 0;
 
 	// Set the timeline fill/cursor position with the most up-to-date value when mpd_status changes
 	useEffect(() => {
-		elapsed.current = position * 1000;
-		setElapsedSeconds(Math.round((elapsed.current / 1000)));
+		elapsed_ms.current = position * 1000;
+		setElapsedSeconds(position);
 	}, [position]);
+
+	useEffect(() => {
+		play_state.current = playState;
+	}, [playState]);
 
 	// Advance the timeline fill/cursor position every frame
 	useEffect(() => {
-		const updateTimeline: FrameRequestCallback = (time) => {
-			const delta = time - last.current;
-			if (playState == PlaybackStatus.Playing) {
-				elapsed.current += delta;
-				if (elapsed.current > (duration ?? elapsed.current)) {
+		if (!realtime) return;
+		const updateTimeline: FrameRequestCallback = () => {
+			const delta_ms = performance.now() - last_ms.current;
+			if (play_state.current == PlaybackStatus.Playing) {
+				elapsed_ms.current += delta_ms;
+				if (duration_seconds) if (elapsed_ms.current > duration_seconds * 1000) {
 					refresh_mpv();
 				}
-				setElapsedSeconds(Math.round((elapsed.current / 1000)));
+				setElapsedSeconds(elapsed_ms.current / 1000);
 			}
-			frame.current = requestAnimationFrame(updateTimeline);
-			last.current = performance.now();
+			// frame.current = requestAnimationFrame(updateTimeline);
+			last_ms.current += delta_ms;
 		};
-		frame.current = requestAnimationFrame(updateTimeline);
-		return () => cancelAnimationFrame(frame.current);
-	}, [duration, position, playState]);
+		frame.current = setInterval(updateTimeline, 100);
+		return () => clearInterval(frame.current);
+	}, [duration_seconds, realtime]);
 
 	return (
 		<div className={mini ? "timeline small" : "timeline"}>
-			<span>T {toHMS(elapsedSeconds)}</span>
+			<span>T {toHMS(elapsed_seconds)}</span>
 			<div className="bar">
-				<div className="fill" style={{ width: duration ? `${(elapsedSeconds / duration) * 100}%` : "100%" }} />
+				<div className="fill" style={{ width: duration_seconds ? `${(elapsed_seconds / duration_seconds) * 100}%` : "100%" }} />
 			</div>
-			<span>-{toHMS(remaining)}</span>
+			<span>-{toHMS(remaining_seconds)}</span>
 		</div>
 	);
 }
