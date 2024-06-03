83843f245674b61e88a9976c227fccc85c9ef8db
Madeline H <maddie@rykan.net>
2024-06-03 17:33:47 +0100

    update home screen content.. sort of

--- vsh/src/components/XB/content-fetcher.ts
+++ vsh/src/components/XB/content-fetcher.ts
@@ -4,11 +4,15 @@ import type { JSX } from "preact";
 import type { ContentItem } from "../Content/types";
 import api, { jellyfin } from "../../context/Jellyfin";
 
-export type XBItem = ContentItem & { Icon?: (() => JSX.Element) | string; desc?: string; };
+export interface XBItem extends ContentItem {
+	Icon?: (() => JSX.Element) | string;
+	desc?: string;
+};
 
-type CategoryContent = {
+export type CategoryContent = {
 	content: Array<XBItem>;
 	default_item?: number;
+	error?: string;
 };
 
 export async function getXBarContent([_, category]: ["xb-category", string]): Promise<CategoryContent> {
@@ -17,26 +21,39 @@ export async function getXBarContent([_, category]: ["xb-category", string]): Pr
 		case "social":
 		case "games":
 			return { content: [] };
+		case "services":
+			return {
+				content: [
+					{ id: "tv.dropout", name: "Dropout", desc: "Hopefully available in future?" },
+					{ id: "tv.nebula", name: "Nebula", desc: "Yeah... IDK" },
+					// { id: "org.invidious", name: "Youtube", desc: "Via Invidious\nComing soon!", Icon: "/invidious-colored-vector.svg" },
+					{ id: "org.invidious", name: "Youtube", desc: "Via Invidious – Coming soon!", Icon: "/xb-icons/youtube/white/youtube_social_squircle_white.png" },
+				]
+			};
 		case "video":
 			return getXBarVideoContent();
+		case "livetv":
+			return getXBarLiveTVContent();
 		case "tv":
 			return getXBarTVContent();
 		case "music":
 			return getXBarMusicContent();
+		case "photos":
+			return getXBarPhotoContent();
 		case "settings":
 			return {
 				default_item: 1,
 				content: [
-					{ id: "system.settings.update", name: "Check for Updates", Icon: "/xb-icons/icon_update.png" },
-					{ id: "system.settings.music", name: "Music Settings", Icon: "/xb-icons/icon_music_setting.png" },
-					{ id: "system.settings.theme", name: "Theme Settings", Icon: "/xb-icons/icon_theme_setting.png" },
-					{ id: "system.settings.video", name: "Video Settings", Icon: "/xb-icons/icon_bdvd_setting.png" },
-					{ id: "system.settings.server", name: "Media Server Connection Settings", Icon: "/xb-icons/icon_network_setting.png" },
-					{ id: "system.settings.sound", name: "Sound Settings", Icon: "/xb-icons/icon_sound_setting.png" },
-					{ id: "system.settings.display", name: "Display Settings", Icon: "/xb-icons/icon_display_setting.png" },
+					{ id: "system.settings.update", name: "Check for Updates", Icon: "/xb-icons/setting/tex_update.png", desc: "Connect to the internet and check for Tellyfin software updates." },
+					{ id: "system.settings.music", name: "Music Settings", Icon: "/xb-icons/setting/tex_music.png", desc: "Adjusts settings for music playback from Audio CD, Alto™, and Jellyfin." },
+					{ id: "system.settings.theme", name: "Theme Settings", Icon: "/xb-icons/setting/tex_theme.png", desc: "Adjusts theme settings for Tellyfin, including icons, background, and fonts." },
+					{ id: "system.settings.video", name: "Video Settings", desc: "Adjusts settings for the MPV video player.", Icon: "/xb-icons/setting/tex_bddvd.png" },
+					{ id: "system.settings.server", name: "Media Server Connection Settings", desc: "Adjust settings for existing media server connections or connect to additional media servers.", Icon: "/xb-icons/setting/tex_network.png" },
+					{ id: "system.settings.sound", name: "Sound Settings", Icon: "/xb-icons/setting/tex_sound.png", desc: "Adjusts settings for audio devices and output formats." },
+					{ id: "system.settings.display", name: "Display Settings", Icon: "/xb-icons/setting/tex_display.png", desc: "Adjusts settings for display resolution and frame rate." },
 				]
 			};
-		case "power":
+		case "system":
 			return {
 				content: [
 					{ id: "system.power.shutdown", name: "Power Off", Icon: "/xb-icons/icon_power_off.png", desc: "Close all applications and turn off the system. Select this before unplugging the AC power cord." },
@@ -50,9 +67,10 @@ export async function getXBarContent([_, category]: ["xb-category", string]): Pr
 }
 
 async function getXBarVideoContent(): Promise<CategoryContent> {
-	const tvLibraries = await jellyfin.getLibraryApi(api).getMediaFolders().then(value => value.data.Items?.filter(item => {
+	const filmLibraries = await jellyfin.getLibraryApi(api).getMediaFolders().then(value => value.data.Items?.filter(item => {
 		switch (item.CollectionType) {
-			case "playlists":
+			case "homevideos":
+			case "boxsets":
 			case "movies":
 				return true;
 			default:
@@ -62,10 +80,11 @@ async function getXBarVideoContent(): Promise<CategoryContent> {
 	return {
 		default_item: 0,
 		content: [
-			...tvLibraries.map(item => ({
+			...filmLibraries.map(item => ({
 				name: item.Name ?? "Unknown",
+				desc: item.Overview ?? undefined,
 				id: item.Id!,
-				Icon: item.CollectionType ? item.CollectionType == "playlists" ? "/xb-icons/icon_playlist.png" : "/xb-icons/icon_video_album_default.png" : undefined,
+				Icon: item.CollectionType ? item.CollectionType == "playlists" ? "/xb-icons/tex/tex_playlist.png" : "/xb-icons/tex/video_tex_album_default.png" : undefined,
 				jellyfin: true,
 				jellyfin_data: item,
 			}))
@@ -90,8 +109,9 @@ async function getXBarTVContent(): Promise<CategoryContent> {
 		content: [
 			...tvLibraries.map(item => ({
 				name: item.Name ?? "Unknown",
+				desc: item.Overview ?? undefined,
 				id: item.Id!,
-				Icon: item.CollectionType ? item.CollectionType == "playlists" ? "/xb-icons/icon_playlist.png" :  "/xb-icons/icon_video_album_default.png" : undefined,
+				Icon: item.CollectionType ? item.CollectionType == "playlists" ? "/xb-icons/tex/tex_playlist.png" : "/xb-icons/tex/video_tex_album_default.png" : undefined,
 				jellyfin: true,
 				jellyfin_data: item,
 			}))
@@ -100,14 +120,82 @@ async function getXBarTVContent(): Promise<CategoryContent> {
 }
 
 async function getXBarMusicContent(): Promise<CategoryContent> {
+	const musicLibraries = await jellyfin.getLibraryApi(api).getMediaFolders().then(value => value.data.Items?.filter(item => {
+		switch (item.CollectionType) {
+			case "playlists":
+			case "music":
+				return true;
+			default:
+				return false;
+		}
+	})) ?? [];
 	return {
-		default_item: 0,
+		default_item: 1,
 		content: [
 			{
-				name: "Playlists",
-				id: "playlists",
-				Icon: "/xb-icons/icon_playlist.png",
-			}
+				name: "Playlists (Alto)",
+				id: "music.alto.playlists",
+				Icon: "/xb-icons/tex/tex_playlist.png",
+			},
+			...musicLibraries.map(item => ({
+				name: item.Name ?? "Unknown",
+				desc: item.Overview ?? undefined,
+				id: item.Id!,
+				Icon: item.CollectionType ? item.CollectionType == "playlists" ? "/xb-icons/tex/tex_playlist.png" : "/xb-icons/tex/music_tex_album_default.png" : undefined,
+				jellyfin: true,
+				jellyfin_data: item,
+			}))
+		]
+	};
+}
+
+async function getXBarPhotoContent(): Promise<CategoryContent> {
+	const photoLibraries = await jellyfin.getLibraryApi(api).getMediaFolders().then(value => value.data.Items?.filter(item => {
+		switch (item.CollectionType) {
+			case "playlists":
+			case "photos":
+				return true;
+			default:
+				return false;
+		}
+	})) ?? [];
+	const default_item = photoLibraries.findIndex(item => item.CollectionType != "playlists");
+	return {
+		default_item: default_item > 0 ? default_item : 0,
+		content: [
+			...photoLibraries.map(item => ({
+				name: item.Name ?? "Unknown",
+				desc: item.Overview ?? undefined,
+				id: item.Id!,
+				Icon: item.CollectionType ? item.CollectionType == "playlists" ? "/xb-icons/tex/tex_playlist.png" : "/xb-icons/tex/photo_tex_album_default.png" : undefined,
+				jellyfin: true,
+				jellyfin_data: item,
+			}))
+		]
+	};
+}
+
+async function getXBarLiveTVContent(): Promise<CategoryContent> {
+	const liveTVSources = await jellyfin.getLibraryApi(api).getMediaFolders().then(value => value.data.Items?.filter(item => {
+		switch (item.CollectionType) {
+			case "livetv":
+				return true;
+			default:
+				return false;
+		}
+	})) ?? [];
+	const default_item = liveTVSources.findIndex(item => item.CollectionType != "playlists");
+	return {
+		default_item: default_item > 0 ? default_item : 0,
+		content: [
+			...liveTVSources.map(item => ({
+				name: item.Name ?? "Unknown",
+				desc: item.Overview ?? undefined,
+				id: item.Id!,
+				Icon: item.CollectionType ? item.CollectionType == "playlists" ? "/xb-icons/tex/tex_playlist.png" : "/xb-icons/tex/photo_tex_album_default.png" : undefined,
+				jellyfin: true,
+				jellyfin_data: item,
+			}))
 		]
 	};
 }
--- vsh/src/home-categories.ts
+++ vsh/src/home-categories.ts
@@ -1,6 +1,21 @@
 import type { XBCategoryData } from "./components/XB";
 
 export const categories: XBCategoryData[] = [
+	// {
+	// 	key: "system",
+	// 	icon: "/xb-icons/icon_user.png",
+	// 	name: "System",
+	// },
+	{
+		key: "settings",
+		icon: "/xb-icons/icon_setting.png",
+		name: "Settings",
+	},
+	{
+		key: "photos",
+		icon: "/xb-icons/icon_photo.png",
+		name: "Photos",
+	},
 	{
 		key: "music",
 		icon: "/xb-icons/icon_music.png",
@@ -9,12 +24,22 @@ export const categories: XBCategoryData[] = [
 	{
 		key: "video",
 		icon: "/xb-icons/icon_video.png",
-		name: "Film & Video",
+		name: "Video",
 	},
 	{
 		key: "tv",
-		icon: "/xb-icons/icon_tv.png",
+		icon: "/xb-icons/TVICON4-NOCOLOUR.png",
 		name: "TV & Video",
+	},
+	{
+		key: "services",
+		icon: "/xb-icons/icon_streaming.png",
+		name: "Video Streaming",
+	},
+	{
+		key: "livetv",
+		icon: "/xb-icons/icon_tv.png",
+		name: "Live TV",
 	},
 	{
 		key: "games",
@@ -25,15 +50,5 @@ export const categories: XBCategoryData[] = [
 		key: "network",
 		icon: "/xb-icons/icon_network.png",
 		name: "Network",
-	},
-	{
-		key: "settings",
-		icon: "/xb-icons/icon_setting.png",
-		name: "Settings",
-	},
-	{
-		key: "power",
-		icon: "/xb-icons/icon_power_off.png",
-		name: "Power",
 	},
 ]
