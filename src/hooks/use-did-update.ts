332408e6b9ba446abce0818fcfba60d8df5876fd
Madeline H <maddie@rykan.net>
2024-07-03 18:14:15 +0100

    I *think* this is correct. I wish React just had an official way to do "useEffect but only on subsequent renders"

--- src/hooks/use-did-update.ts
+++ src/hooks/use-did-update.ts
@@ -1,13 +1,29 @@
-import { useRef, useEffect } from "preact/hooks";
+import { useRef, useEffect, useLayoutEffect } from "preact/hooks";
 
 export function useDidUpdate(fn: () => (void | (() => void)), deps: Array<any>) {
-	const isMountingRef = useRef(false);
+	const isMountingRef = useRef(true);
 
 	useEffect(() => {
 		isMountingRef.current = true;
 	}, []);
 
 	useEffect(() => {
+		if (!isMountingRef.current) {
+			return fn();
+		} else {
+			isMountingRef.current = false;
+		}
+	}, deps); // eslint-disable-line react-hooks/exhaustive-deps
+}
+
+export function useDeferredLayoutEffect(fn: () => (void | (() => void)), deps: Array<any>) {
+	const isMountingRef = useRef(true);
+
+	useLayoutEffect(() => {
+		isMountingRef.current = true;
+	}, []);
+
+	useLayoutEffect(() => {
 		if (!isMountingRef.current) {
 			return fn();
 		} else {
