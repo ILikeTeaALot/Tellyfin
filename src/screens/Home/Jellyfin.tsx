161a21918a026201ee70958eca5a7c98ca7e50ab
Madeline H <maddie@rykan.net>
2024-07-03 17:04:19 +0100

    remove dead code

--- src/screens/Home/Jellyfin.tsx
+++ src/screens/Home/Jellyfin.tsx
@@ -17,28 +17,6 @@ export type JellyfinContentProps = {
 export function JellyfinContent(props: JellyfinContentProps) {
 	const { nav_position } = props;
 	const state = useContext(AppMode);
-	// Handle keeping onNavigate callback... correct.
-	// const onNavigate = useRef(props.onNavigate);
-	// onNavigate.current = props.onNavigate;
-	// Normal stuff
-	// const [selected, setSelected] = useState(0);
-	// useEffect(() => {
-	// 	if (position == 0) {
-	// 		function handler(e: KeyboardEvent) {
-	// 			console.log(e.key);
-	// 			switch (e.key) {
-	// 				case "Backspace":
-	// 				case "Back":
-	// 					onNavigate.current(NavigateAction.Back);
-	// 					break;
-	// 				default:
-	// 					break;
-	// 			}
-	// 		}
-	// 		window.addEventListener("keydown", handler);
-	// 		return () => { window.removeEventListener("keydown", handler); };
-	// 	}
-	// }, [onNavigate]);
 	if (typeof props.data.jellyfin_data == "undefined") {
 		return (
 			<div>No content</div>
