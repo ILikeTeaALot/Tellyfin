a4b0658548b173368268800ae67c0d9584d1480b
Madeline H <maddie@rykan.net>
2024-07-14 07:56:33 +0100

    use theme icons in unused function

--- src/components/XB/list-content-fetcher.ts
+++ src/components/XB/list-content-fetcher.ts
@@ -30,19 +30,19 @@ export async function getSettingsContent(key: string, settings: UserSettings): P
 		case "music":
 			return {
 				content: [
-					{ id: "system.settings:music.preferred_library", name: "Preferred Library", desc: "Sets the preferred music library content listed under the [Music] category.", Icon: "/xb-icons/setting/tex_sett.png", /* value: await manager.get("music.preferred_library") */ value: settings?.music?.preferred_library },
+					{ id: "system.settings:music.preferred_library", name: "Preferred Library", desc: "Sets the preferred music library content listed under the [Music] category.", Icon: "icon:settings.item", /* value: await manager.get("music.preferred_library") */ value: settings?.music?.preferred_library },
 					{
 						id: "system.settings:music.cd_import",
 						name: "Audio CD Import",
 						desc: "Sets the codec and bit rate when importing audio CDs.",
-						Icon: "/xb-icons/setting/tex_sett.png",
+						Icon: "icon:settings.item",
 						/* value: "FLAC" */
 						/* value: await manager.get("music.cd_import").then(value => [value.format, value.bitrate ?? value.vbr ? "Variable" : null] as const).then(arr => arr.filter(v => v).join(" ")) */
 						value: [settings.music.cd_import.format, settings.music.cd_import.bitrate ?? settings.music.cd_import.vbr ? "Varable Bitrate" : null].filter(v => v).join(" ")
 					},
-					{ id: "system.settings:music.crossfade", name: "Crossfade Playback", desc: "Sets the amount of time that is crossfaded (overlapped) between music tracks. This setting is used when playing content other than CD Audio.", Icon: "/xb-icons/setting/tex_sett.png", /* value: "Off" */ /* value: await manager.get("music.crossfade") || "Off" */ value: settings?.music?.crossfade == 0 ? "Off" : `${settings?.music?.crossfade} Seconds` },
-					{ id: "system.settings:music.output_freq", name: "Output Frequency", desc: "Sets the audio output frequency when playing music content.\nRecommended: Auto or Upsample", Icon: "/xb-icons/setting/tex_sett.png", /* value: "Auto" */ value: settings?.music?.output_freq },
-					// { id: "system.settings:music.bitmapping", name: "Bitmapping", desc: "Honestly I care about audio but even I don't know what this does. “Set this according to your preference”??? HOW? IT WAS SUPER PAINFUL TO CHANGE MUSIC SETTINGS BETWEEN PLAYING TRACKS ON THE PS3!", Icon: "/xb-icons/setting/tex_sett.png", value: "Type 1" },
+					{ id: "system.settings:music.crossfade", name: "Crossfade Playback", desc: "Sets the amount of time that is crossfaded (overlapped) between music tracks. This setting is used when playing content other than CD Audio.", Icon: "icon:settings.item", /* value: "Off" */ /* value: await manager.get("music.crossfade") || "Off" */ value: settings?.music?.crossfade == 0 ? "Off" : `${settings?.music?.crossfade} Seconds` },
+					{ id: "system.settings:music.output_freq", name: "Output Frequency", desc: "Sets the audio output frequency when playing music content.\nRecommended: Auto or Upsample", Icon: "icon:settings.item", /* value: "Auto" */ value: settings?.music?.output_freq },
+					// { id: "system.settings:music.bitmapping", name: "Bitmapping", desc: "Honestly I care about audio but even I don't know what this does. “Set this according to your preference”??? HOW? IT WAS SUPER PAINFUL TO CHANGE MUSIC SETTINGS BETWEEN PLAYING TRACKS ON THE PS3!", Icon: "icon:settings.item", value: "Type 1" },
 				]
 			};
 	}
