49aa03d7c4ecaf391c62cc61bc3f1f4d027b5816
Madeline H <maddie@rykan.net>
2024-07-28 14:59:29 +0100

    add same tv-show blurred background to film detail

--- src/renderer/vsh/screens/Home/Films/FilmDetail.tsx
+++ src/renderer/vsh/screens/Home/Films/FilmDetail.tsx
@@ -11,6 +11,7 @@ import { displayRunningTime } from "../../../util/functions";
 import { MediaStreamInfo } from "../../../components/Jellyfin/MediaStreamInfo";
 import { useInput } from "../../../hooks";
 import { playFile } from "../../../functions/play";
+import { BackdropImage } from "../TV/Series";
 import { Loading } from "~/renderer/vsh/components/Loading";
 
 const WIDTH = 400;
@@ -86,49 +87,52 @@ export function FilmDetail(props: JellyfinScreenProps) {
 	const runTimeTicks = info.RunTimeTicks;
 	const duration = runTimeTicks ? displayRunningTime(runTimeTicks) : null;
 	return (
-		// <div class={active ? "background-blur active" : "background-blur"}>
-		<div className="fullscreen-mask bottom">
-			<div class="film-info" style={{
-				// filter: nav_position == 0 ? undefined : "blur(60px) saturate(180%)",
-				opacity: nav_position == 0 ? 1 : 0,
-				scale: `${1 + (Math.max(Math.min(nav_position, 1), -1) * -0.2)}`,
-				transitionTimingFunction: nav_position == 0 ? "var(--timing-function-decelerate)" : "var(--timing-function-accelerate)",
-				transitionDelay: nav_position == 0 ? "var(--transition-standard)" : "0ms",
-			}}>
-				<h1 style={{ marginLeft: 80, marginTop: 80 }}>{info.Name ?? "Title Unknown"}</h1>
-				<div className="film-h-split">
-					<div className="film-cover">
-						<ContentPanel state={PanelState.None} aspectRatio={info.PrimaryImageAspectRatio ?? undefined} width={WIDTH} height={WIDTH / (info.PrimaryImageAspectRatio ?? 1.5)}>
-							<img
-								decoding="async"
-								src={`${api.basePath}/Items/${props.data.Id}/Images/Primary`}
-								style={{
-									objectFit: "cover",
-									width: "100%",
-									height: "100%",
-								}}
-							/>
-						</ContentPanel>
-						<h2 class="list-item selected" style={{
-							fontWeight: 700,
-							fontSize: 56,
-							animation: "var(--text-glow)",
-						}}>Play</h2>
-					</div>
-					<div ref={episode_overview} className={row == Row.Overview ? "film-details focused" : "film-details"}>
-						<p style={{ maxWidth: 1200 }}>{info.Overview ?? "No overview available"}</p>
-						{duration ? <span>{duration}</span> : null}
-						{typeof info.PremiereDate == "string" ? (
-							<span>
-								Released {new Date(info.PremiereDate!)
-									.toLocaleDateString(undefined, { weekday: "long", year: "numeric", month: "long", day: "numeric" })}
-							</span>
-						) : null}
-						{info.MediaStreams ? info.MediaStreams.map(info => <MediaStreamInfo info={info} />) : null}
+		<>
+			<BackdropImage index={0} selected={0} item={info} show={active} />
+			<div className="fullscreen-mask bottom">
+				<div class="film-info" style={{
+					// filter: nav_position == 0 ? undefined : "blur(60px) saturate(180%)",
+					opacity: nav_position == 0 ? 1 : 0,
+					// scale: `${1 + (Math.max(Math.min(nav_position, 1), -1) * -0.2)}`,
+					transitionTimingFunction: nav_position == 0 ? "var(--timing-function-decelerate)" : "var(--timing-function-accelerate)",
+					transitionDelay: nav_position == 0 ? "var(--transition-standard)" : "0ms",
+				}}>
+					<h1 style={{ marginLeft: 80, marginTop: 80 }}>{info.Name ?? "Title Unknown"}</h1>
+					<div className="film-h-split">
+						<div className="film-cover">
+							<ContentPanel state={PanelState.None} aspectRatio={info.PrimaryImageAspectRatio ?? undefined} width={WIDTH} height={WIDTH / (info.PrimaryImageAspectRatio ?? 1.5)}>
+								<img
+									decoding="async"
+									src={`${api.basePath}/Items/${props.data.Id}/Images/Primary`}
+									style={{
+										objectFit: "cover",
+										width: "100%",
+										height: "100%",
+									}}
+								/>
+							</ContentPanel>
+							<h2 class="list-item selected" style={{
+								fontWeight: 700,
+								fontSize: 56,
+								animation: "var(--text-glow)",
+							}}>Play</h2>
+						</div>
+						<div ref={episode_overview} className={row == Row.Overview ? "film-details focused" : "film-details"}>
+							<p style={{ maxWidth: 1200 }}>{info.Overview ?? "No overview available"}</p>
+							{duration ? <span>{duration}</span> : null}
+							{typeof info.PremiereDate == "string" ? (
+								<span>
+									Released {new Date(info.PremiereDate!)
+										.toLocaleDateString(undefined, { weekday: "long", year: "numeric", month: "long", day: "numeric" })}
+								</span>
+							) : null}
+							{info.MediaStreams ? info.MediaStreams.map(info => <MediaStreamInfo info={info} />) : null}
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
+		// <div class={active ? "background-blur active" : "background-blur"}>
 		// </div>
 	);
 }
