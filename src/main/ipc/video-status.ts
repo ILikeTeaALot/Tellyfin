bfeb2afc68fb644820887eb30e5773024bc06181
Madeline H <maddie@rykan.net>
2024-07-28 14:56:38 +0100

    current playing item id

--- src/main/ipc/video-status.ts
+++ src/main/ipc/video-status.ts
@@ -1,8 +1,9 @@
 import { ipcMain } from "electron";
 import { status } from "mpv";
+import { current_playing_id } from "../globals";
 
 export function setupVideoStatusHandlers() {
 	ipcMain.handle("mpv_status", (_) => {
-		return status();
+		return { ...status(), mediaType: current_playing_id.inner };
 	});
 }
