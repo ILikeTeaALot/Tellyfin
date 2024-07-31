a3bf295d15a9092435c50c5cfd7d3588ddf1de6b
Madeline H <maddie@rykan.net>
2024-07-31 13:28:49 +0100

    improve control panel icons

--- src/renderer/vsh/components/ControlPanelButton.tsx
+++ src/renderer/vsh/components/ControlPanelButton.tsx
@@ -3,6 +3,6 @@ import { iconByName } from "./Icon";
 export function PanelButton(props: { active: boolean; action: string; }) {
 	const { active, action } = props;
 	return (
-		<div className={active ? "control-panel-button active" : "control-panel-button"}>{iconByName(action)}</div>
+		<div className={active ? "control-panel-button active" : "control-panel-button"}>{iconByName(action, active)}</div>
 	);
 }
--- src/renderer/vsh/components/Icon.tsx
+++ src/renderer/vsh/components/Icon.tsx
@@ -1,23 +1,61 @@
+import "./icon.css";
+
 import { FastForwardIcon, PauseIcon, PlayIcon, RewindIcon, StopIcon } from "../Icons";
 import { JumpNextIcon } from "../Icons/JumpNext";
 import { JumpPreviousIcon } from "../Icons/JumpPrev";
 
-export function iconByName(name: string) {
+export function iconByName(name: string, focused?: boolean) {
 	switch (name) {
 		case "SceneSearch":
-			return "Scenes";
+			// return "Scenes";
+			return (
+				<div class="cp_icon">
+					{/* {focused && <img class="absolute" src="xb-icon://localhost/system/video.panel.scene_search.focus" />} */}
+					<img class="absolute" src="xb-icon://localhost/system/video.panel.scene_search.normal" />
+				</div>
+			);
 		case "GoTo":
-			return "Go To";
+			// return "Go To";
+			return (
+				<div class="cp_icon">
+					{/* {focused && <img class="absolute" src="xb-icon://localhost/system/video.panel.go_to.focus" />} */}
+					<img class="absolute" src="xb-icon://localhost/system/video.panel.go_to.normal" />
+				</div>
+			);
 		case "AudioOptions":
-			return "Audio"
+			// return "Audio";
+			return (
+				<div class="cp_icon">
+					{/* {focused && <img class="absolute" src="xb-icon://localhost/system/video.panel.sound.focus" />} */}
+					<img class="absolute" src="xb-icon://localhost/system/video.panel.sound.normal" />
+				</div>
+			);
 		case "SubtitleOptions":
-			return "Subtitles";
+			// return "Subtitles";
+			return (
+				<div class="cp_icon">
+					{/* {focused && <img class="absolute" src="xb-icon://localhost/system/video.panel.subtitle.focus" />} */}
+					<img class="absolute" src="xb-icon://localhost/system/video.panel.subtitle.normal" />
+				</div>
+			);
 		case "AVSettings":
-			return "AV Settings";
+			// return "AV Settings";
+			return (
+				<div class="cp_icon">
+					{/* {focused && <img class="absolute" src="xb-icon://localhost/system/video.panel.av_settings.focus" />} */}
+					<img class="absolute" src="xb-icon://localhost/system/video.panel.av_settings.normal" />
+				</div>
+			);
 		case "TimeOptions":
 			return "Time Options";
 		case "Display":
-			return "Display";
+			// return "Display";
+			return (
+				<div class="cp_icon">
+					{/* {focused && <img class="absolute" src="xb-icon://localhost/system/video.panel.display.focus" />} */}
+					<img class="absolute" src="xb-icon://localhost/system/video.panel.display.normal" />
+				</div>
+			);
 		// Transport Controls
 		case "PrevChapter":
 			return <JumpPreviousIcon />;
